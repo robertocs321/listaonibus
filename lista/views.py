c383809bd0b59b403118fda4b223348ce68fb453
Roberto Carlos <robertosantoscarlos14@gmail.com>
2019-09-30 14:16:13 -0300

    apagando

--- lista/views.py
+++ lista/views.py
@@ -9,10 +9,7 @@ from datetime import datetime, timedelta
 import os
 # Create your views here.
 
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+
 
 
 def listar (request):
